2d9fae3e68e1b8d7750b52e6df9ffc5385d21328
Israel Morales <israel.morales@rocket.com.mx>
2015-11-16 11:22:10 -0800

    enabled parallax for the homepage

--- themes/custom/d8_materialize/js/d8_materialize/custom.js
+++ themes/custom/d8_materialize/js/d8_materialize/custom.js
@@ -1,3 +1,25 @@
 jQuery(document).ready(function() {
     jQuery('.button-collapse').sideNav();
+
+
+    jQuery('.parallax').parallax();
+
+    // Sticky Navigation.
+
+    //jQuery(window).scroll(function() {
+    //    var topnav = jQuery('.top-nav');
+    //    var navw = jQuery('.nav-wrapper');
+    //    var scroll = jQuery(window).scrollTop();
+    //    if(navw.offset().top < scroll) {
+    //        if(topnav.hasClass('sticky') === false) {
+    //            topnav.addClass('sticky');
+    //        } else {
+    //            return;
+    //        }
+    //    } else if(navw.offset().top > scroll) {
+    //        topnav.removeClass('sticky');
+    //    }
+    //});
+
+
 });
