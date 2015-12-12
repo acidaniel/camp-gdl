129f16493c05a7f2315dda707165e57d361266a6
Israel Morales <israel.morales@rocket.com.mx>
2015-12-11 18:49:16 -0800

    reverse counter

--- themes/custom/d8_materialize/js/d8_materialize/custom.js
+++ themes/custom/d8_materialize/js/d8_materialize/custom.js
@@ -24,5 +24,36 @@ jQuery(document).ready(function() {
 
     jQuery('.nav-wrapper > ul').removeClass('right');
 
+    // Backwards counter.
+    origin = moment("2016-04-07 09:00:00", "YYYY-MM-DD HH:mm:ss");
+    daysObject = document.getElementsByClassName('days-counter');
+    hoursObject = document.getElementsByClassName('hours-counter');
+    minutesObject = document.getElementsByClassName('minutes-counter');
+    secondsObject = document.getElementsByClassName('second-counter');
+    function calculateTime() {
+
+        days = moment().diff(origin, 'days');
+        hours = moment().diff(origin, 'hours');
+        hours = hours-(days*24);
+
+        minutes = moment().diff(origin, 'minutes');
+        minutes = minutes-(days*1440)-(hours*60);
+
+        seconds = moment().diff(origin, 'seconds');
+        seconds -= (days*86400);
+        seconds -= (hours*3600);
+        seconds -= (minutes*60);
+
+        daysObject[0].innerText = -(days);
+        hoursObject[0].innerText = -(hours);
+        minutesObject[0].innerText = -(minutes);
+        secondsObject[0].innerText = -(seconds);
+
+    }
+    setInterval(function() {
+        calculateTime();
+    }, 1000);
+
+
 
 });
